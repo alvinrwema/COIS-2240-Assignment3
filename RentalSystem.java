79d2126ee19520899a1c1b86a0e22e5d66371222
alvinrwema <alvinrwema.01@gmail.com>
2025-11-29 23:25:36 -0500

    Addition of loadData method (Subtask 3)

--- RentalSystem.java
+++ RentalSystem.java
@@ -10,7 +10,9 @@ import java.util.ArrayList;
 public class RentalSystem {
 	
 	private static RentalSystem instance = new RentalSystem(); //Holds the instance of the rental system and initialises it.
-	private RentalSystem() {}
+	private RentalSystem() {
+		loadData(); //Calls the loadData method so that all the saved details are loaded.
+	}
 	
 	public static RentalSystem getInstance() {
 		return instance; //Returns the instance of rentalSystem to work with.
@@ -211,7 +213,124 @@ public class RentalSystem {
     		e.printStackTrace();
     	}
         	
-   }
+     }
+     
+     private void loadData() {
+     	
+     	loadVehicles();  //Calls this method so vehicles are loaded from vehicles.txt
+     	loadCustomers();  //Calls this method so customers are loaded from customer.txt
+     	loadRecords();  //Calls this method so records are loaded from rental_records.txt
+     }
+     
+     private void loadVehicles() {
+     	try {
+ 			BufferedReader reader = new BufferedReader(new FileReader("vehicles.txt"));
+ 			String line;
+ 			while((line = reader.readLine()) != null) {
+ 				//Moves to next line if the line is empty
+ 				 if (line.trim().isEmpty()) {
+ 				        continue;
+ 				    }
+ 				
+ 				String[] data = line.split(",");   //Reads in the data of the line into any array by splitting it with a comma
+ 				
+ 				//Gets the necessary fields of the vehicle using the order in which they were saved.
+ 				String type = data[0];  
+ 				String licensePlate = data[1];
+ 				String make = data[2];
+ 				String model = data[3];
+ 				int year =Integer.parseInt(data[4]);
+ 				Vehicle vehicle = null;
+ 				
+ 				if(type.equals("Car")) {
+ 					int numSeats = Integer.parseInt(data[5]);
+ 					vehicle = new Car(make,model,year,numSeats);
+ 				}
+ 				else if(type.equals("Minibus")) {
+ 					boolean isAccessible = Boolean.parseBoolean(data[5]);
+ 					vehicle = new Minibus(make,model,year,isAccessible);
+ 				}
+ 				else {
+ 					double cargoSize = Double.parseDouble(data[5]);
+ 					boolean hasTrailer = Boolean.parseBoolean(data[6]);
+ 					vehicle = new PickupTruck(make,model,year,cargoSize,hasTrailer);
+ 				}
+ 				
+ 				if(vehicle != null) {
+ 					vehicle.setLicensePlate(licensePlate);
+ 					vehicles.add(vehicle); //Adds the vehicle to the list 
+ 				}	
+ 			}
+ 			reader.close();
+ 		} catch (IOException e) {
+ 			e.printStackTrace();
+ 		}
+     }
+     private void loadCustomers() {
+     	try {
+ 			BufferedReader reader = new BufferedReader(new FileReader("customers.txt"));
+ 			String line;
+ 			while((line = reader.readLine()) != null) {
+ 				//Moves to next line if the line is empty
+ 				 if (line.trim().isEmpty()) {
+ 				        continue;
+ 				    }
+ 				//Reads in the data of the line into any array by splitting it with a comma
+ 				String[] data = line.split(",");
+ 				
+ 				//Gets both the customer id and name details from the array
+ 				int customerId = Integer.parseInt(data[0]);
+ 				String customerName = data[1];
+ 				
+ 				Customer customer = new Customer(customerId,customerName);  //Creates a customer object using details read from the line
+ 				customers.add(customer); //Adds the customer to the list
+ 			}
+ 			reader.close();
+ 		} catch (IOException e) {
+ 			e.printStackTrace();
+ 		}
+     }
+     private void loadRecords() {
+     	try {
+ 			BufferedReader reader = new BufferedReader(new FileReader("rental_records.txt"));
+ 			String line;
+ 			while((line = reader.readLine()) != null) {
+ 				//Moves to next line if the line is empty
+ 			    if (line.trim().isEmpty()) {
+ 			       continue;
+ 			    }
+ 				String[] data = line.split(",");
+ 				
+ 				//Finds the vehicle and customer objects using licensePlate and customerId.
+ 				Vehicle vehicle = findVehicleByPlate(data[0]);
+ 				Customer customer = findCustomerById(Integer.parseInt(data[1]));
+ 				
+ 				//Checks if either objects are null
+ 				if(vehicle == null || customer == null) {
+ 					continue; //Moves to next line to prevent any errors
+ 				}
+ 				LocalDate recordDate = LocalDate.parse(data[2]);
+ 				double amount = Double.parseDouble(data[3]);
+ 				String recordType = data[4];
+ 				
+ 				RentalRecord record =  new RentalRecord(vehicle,customer,recordDate,amount,recordType); //Creates record object using all the fields.
+ 				rentalHistory.addRecord(record); //Adds record to rentalHistory
+ 				
+ 				//Sets the status of the vehicle depending on the recordType
+ 	            if (recordType.equals("RENT")) {
+ 	                vehicle.setStatus(Vehicle.VehicleStatus.Rented);
+ 	            } 
+ 	            else if (recordType.equals("RETURN")) {
+ 	                vehicle.setStatus(Vehicle.VehicleStatus.Available);
+ 	            }
+ 				
+ 			
+ 			}
+ 			reader.close();
+ 		} catch (IOException e) {
+ 			e.printStackTrace();
+ 		}
+     }
     	
     
 
